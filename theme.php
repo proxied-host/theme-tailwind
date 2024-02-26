576fabcdb76623f6a49ce8a1e6ece34e7884cfc6
djoh <djoh@proxied.host>
2024-02-26 10:26:53 +0100

    Update theme name and author in theme.php

--- theme.php
+++ theme.php
@@ -2,13 +2,13 @@
 
 return [
 
-    'name' => 'Tailwind',
+    'name' => 'Tailwind-custom',
     'version' => '1.0.0',
     'wemx_version' => '1.0.0', 
-    'author' => 'Mubeen',
+    'author' => 'Mubeen & djoh',
 
     // Below are assets for the theme
-    'folder' => 'tailwind',
+    'folder' => 'tailwind-custom',
 
     // Wrapper are used by addon developers to display the contents of a module
     // A wrapper should be a blank page with menu, user settings around it
